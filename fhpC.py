6f26c9eff5e4d5ce03a18f49c2555579b7e37df9
Zenitsukkjs <159107264+Zenitsukkjs@users.noreply.github.com>
2024-02-08 20:21:38 -0300

    Atualizando arquivo via API

--- fhpC.py
+++ fhpC.py
@@ -1,1 +1,33 @@
-a
+#########################################################################################
+import os , socket , threading , json , requests
+from time import sleep
+#########################################################################################
+
+
+#########################################################################################
+def __ClientSockt__():
+    while(True):
+            
+        try:
+            global ClientSockt
+            ClientSockt = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+            ClientSockt.connect((Host , Port))
+            print(f"Conectado > {Host} : {Port}")
+            data =  ClientSockt.recv(1024)
+            if data :
+                print(data)
+                break
+            else:pass
+            
+        except:pass
+#########################################################################################
+
+
+#########################################################################################
+def __Start__():
+    threading.Thread( target= __ClientSockt__ ).start()
+if __name__ == "__main__":
+    __Start__()
+#########################################################################################
+IP = "0.tcp.sa.ngrok.io"
+PORT = 14161
